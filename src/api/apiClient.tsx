861233ef9bc3196ea41ab271c4b7132500495685
kr254na <krishnaagarwal0193@gmail.com>
2026-05-18 04:18:43 +0530

    Done

--- src/api/apiClient.tsx
+++ src/api/apiClient.tsx
@@ -2,7 +2,7 @@ const BASE_URL = 'https://agrosmart-backend-spz5.onrender.com';
 //const BASE_URL = 'http://localhost:8081';
 import { getStorage, setStorage, clearAllStorage } from '../utils/storage';
 
-// Internal state to handle multiple simultaneous 401s
+
 let isRefreshing = false;
 let refreshSubscribers: ((token: string) => void)[] = [];
 
